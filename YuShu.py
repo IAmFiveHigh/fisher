5f95b4726d4162efe865a41ddb4173a4c40b9de2
five_high <z393533493@163.com>
2018-08-19 10:57:39 +0800

    添加yushu api接口

---
+++ YuShu.py
@@ -0,0 +1,20 @@
+from http import HTTP
+
+
+class YuShu:
+    isbn_url = 'http://t.yushu.im/v2/book/isbn/{}'
+    keyword_url = 'http://t.yushu.im/v2/book/search?q={}&count={}&start={}'
+
+    @classmethod
+    def search_by_isbn(cls, isbn):
+        url = cls.isbn_url.format(isbn)
+        result = HTTP.get(url)
+        return result
+
+    @classmethod
+    def search_by_keyword(cls, key, count=15, start=0):
+        url = cls.keyword_url.format(key, count, start)
+        result = HTTP.get(url)
+        return result
+
+
