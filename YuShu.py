b918511fcebb6f5391e2b71a618595528077945b
five_high <z393533493@163.com>
2018-08-21 14:53:50 +0800

    修改文件结构 完成book/Search接口验证参数

--- YuShu.py
+++ YuShu.py
@@ -1,7 +1,8 @@
 from httper import HTTP
-
+from flask import current_app
 
 class YuShu:
+
     isbn_url = 'http://t.yushu.im/v2/book/isbn/{}'
     keyword_url = 'http://t.yushu.im/v2/book/search?q={}&count={}&start={}'
 
@@ -12,9 +13,12 @@ class YuShu:
         return result
 
     @classmethod
-    def search_by_keyword(cls, key, count=15, start=0):
-        url = cls.keyword_url.format(key, count, start)
+    def search_by_keyword(cls, key, page=1):
+
+        url = cls.keyword_url.format(key, current_app.config["PER_PAGE"], cls.calculate_start(page))
         result = HTTP.get(url)
         return result
 
-
+    @staticmethod
+    def calculate_start(page):
+        return (page-1) * current_app.config["PER_PAGE"]
