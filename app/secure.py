636da45b3248165065c6ffb5e7640f3b7f9cca0d
five_high <z393533493@163.com>
2018-09-18 16:47:33 +0800

    隐藏secure.py

--- app/secure.py
+++
@@ -1,5 +0,0 @@
-
-
-DEBUG = True
-
-SQLALCHEMY_DATABASE_URI = 'mysql+cymysql://root:123456789@localhost:3306/fisher'
---
+++ test/text.py
@@ -0,0 +1,5 @@
+"""
+  created by wugao on 2018-08-23
+ """
+
+__author__ = "IAmFiveHigh"
